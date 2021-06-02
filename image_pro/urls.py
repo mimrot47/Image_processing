e91955e3dc85c469ec4db6ce0ed16e860cd2fd78
gokul <mimrot47@gmail.com>
2021-06-02 13:21:08 +0530

    setting .py change

--- image_pro/urls.py
+++ image_pro/urls.py
@@ -18,13 +18,10 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from process import views
-from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home),
-    # url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    path('',views.home), 
 ]
 
 urlpatterns += static(
